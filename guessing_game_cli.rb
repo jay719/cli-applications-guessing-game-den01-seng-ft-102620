930d02ddc500522fa2413aef58c1d67b1f11ca21
Javaria Brascom <javariab17@gmail.com>
2020-10-26 07:07:48 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,15 @@
 # Code your solution here!
+def run_guessing_game
+random= rand(6) + 1
+input= gets.chomp
+if input =="exit"
+  puts "Goodbye!"
+elsif random==input
+  puts"You guessed the correct number!"
+else 
+  "Sorry! The computer guessed #{random}."
+end
+end
+  
+  
+  
